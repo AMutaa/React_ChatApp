a90af032ec6adcb577214d79bdc1fdda026ac1e4
AMutaa <piusandru@gmail.com>
2018-10-01 09:15:37 -0400

    rendering components

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
 import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Title />
+        <MessageList />
+        <SendMessageList />
+      </div>
+    );
+  }
+}
+
+export default App;
